2f3a334046a8b32023998d7f6b12dfeae8a13c8f
gruenean <gruenean@students.zhaw.ch>
2012-03-27 11:13:04 +0200

    validation for url, price and title

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
  validates :title, :description, :image_url, :presence => true
+ validates :price, :numerically => {:greater_than_or_equal_to => 0.01}
+ validates :title, :uniqueness => true
+ validates :image_url, format => {
+   :with => %r{\.(gif|jpe?g|png)$}i,
+   :message => 'must be an URL for GIF, JPG, JPEG or PNG'
+ }
 end
